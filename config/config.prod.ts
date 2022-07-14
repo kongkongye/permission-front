2226a32ec4acbd44b737084c844633190230a403
kongkongye <kongkongye@163.com>
2022-07-14 15:29:31 +0800

    fix

--- config/config.prod.ts
+++ config/config.prod.ts
@@ -2,6 +2,6 @@ import { defineConfig } from '@umijs/max';
 
 export default defineConfig({
   define: {
-    API_URL: 'http://sjcj.wxgx.com:9851/permission-api',
+    API_URL: 'https://sjcj.wxgx.com:9851/permission-api',
   },
 });
