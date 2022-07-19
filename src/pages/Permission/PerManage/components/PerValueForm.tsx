e3077a9bbed52d8b03e2f98ccd2d4877b30f896a
kongkongye <kongkongye@163.com>
2022-07-19 11:45:42 +0800

    filterCode

--- src/pages/Permission/PerManage/components/PerValueForm.tsx
+++ src/pages/Permission/PerManage/components/PerValueForm.tsx
@@ -1,4 +1,4 @@
-import {ProFormText, ProFormTextArea, ProDescriptions} from '@ant-design/pro-components';
+import { ProFormText, ProFormTextArea, ProDescriptions } from '@ant-design/pro-components';
 
 type Props = {
   type?: string;
@@ -7,17 +7,22 @@ type Props = {
 };
 
 const Index: React.FC<Props> = (props: Props) => {
-  const {type, parent, isEdit=false} = props;
+  const { type, parent, isEdit = false } = props;
 
   return (
     <>
       <ProDescriptions>
-        <ProDescriptions.Item label='权限类型' contentStyle={{color: type?'inherit':'grey'}}>{type??'无'}</ProDescriptions.Item>
-        <ProDescriptions.Item label='父权限' contentStyle={{color: parent?'inherit':'grey'}}>{parent?.name??'无'}</ProDescriptions.Item>
+        <ProDescriptions.Item label="权限类型" contentStyle={{ color: type ? 'inherit' : 'grey' }}>
+          {type ?? '无'}
+        </ProDescriptions.Item>
+        <ProDescriptions.Item label="父权限" contentStyle={{ color: parent ? 'inherit' : 'grey' }}>
+          {parent?.name ?? '无'}
+        </ProDescriptions.Item>
       </ProDescriptions>
-      <ProFormText name="id" hidden/>
-      <ProFormText name="typeCode" hidden/>
-      <ProFormText name="parent" hidden/>
+      <ProFormText name="id" hidden />
+      <ProFormText name="typeCode" hidden />
+      <ProFormText name="parent" hidden />
+      <ProFormText name="filterCode" hidden />
       <ProFormText name="name" label="名称" placeholder="输入名称" width="md" />
       <ProFormText name="code" label="编码" placeholder="输入编码" width="md" disabled={isEdit} />
       <ProFormTextArea name="note" label="备注" placeholder="输入备注" />
--- src/pages/Permission/PerManage/index.tsx
+++ src/pages/Permission/PerManage/index.tsx
@@ -1,55 +1,54 @@
 import EditForm from '@/components/EditForm';
-import {getPerValue, listPerTypes, savePerValue} from '@/services/permission/api';
+import { getPerValue, listPerTypes, savePerValue } from '@/services/permission/api';
 import {
-  ProForm,
   ProFormGroup,
   ProFormInstance,
   ProFormSelect,
   ProFormSwitch,
-  ProFormTreeSelect
+  ProFormTreeSelect,
 } from '@ant-design/pro-components';
-import {Button, Form, Layout, message, Popover, Tree, TreeSelect} from 'antd';
-import {Content} from 'antd/es/layout/layout';
-import {Key} from 'antd/es/table/interface';
-import React, {useCallback, useEffect, useMemo, useRef, useState} from 'react';
+import { Button, Form, Layout, message, Popover, Tree, TreeSelect } from 'antd';
+import { Content } from 'antd/es/layout/layout';
+import { Key } from 'antd/es/table/interface';
+import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react';
 import styles from './index.less';
-import usePerValueBriefs from "@/hooks/usePerValueBriefs";
-import PerValueForm from "@/pages/Permission/PerManage/components/PerValueForm";
+import usePerValueBriefs from '@/hooks/usePerValueBriefs';
+import PerValueForm from '@/pages/Permission/PerManage/components/PerValueForm';
 
 const PerManage: React.FC = () => {
   const [perTypes, setPerTypes] = useState({});
   const [typeCode, setTypeCode] = useState<string>();
   useEffect(() => {
-    listPerTypes({}).then(res => {
+    listPerTypes({}).then((res) => {
       if (res) {
-        const perTypes_ = {}
+        const perTypes_ = {};
         for (const e of res) {
           perTypes_[e.code] = {
             text: e.name,
             filter: e.filter,
-          }
+          };
         }
-        setPerTypes(perTypes_)
+        setPerTypes(perTypes_);
       } else {
-        setPerTypes({})
+        setPerTypes({});
       }
-    })
-  }, [])
+    });
+  }, []);
   const filterTypeCode = useMemo(() => {
     if (typeCode) {
-      const perType = perTypes[typeCode]
-      return perType?.filter
+      const perType = perTypes[typeCode];
+      return perType?.filter;
     }
-  }, [typeCode, perTypes])
+  }, [typeCode, perTypes]);
   const [filterCode, setFilterCode] = useState<string>();
   const [filterContainSub, setFilterContainSub] = useState<boolean>(false);
 
-  const {codes, treeData, refresh} = usePerValueBriefs(typeCode, filterCode, filterContainSub)
-  const {treeData: filterTreeData} = usePerValueBriefs(filterTypeCode)
+  const { codes, treeData, refresh } = usePerValueBriefs(typeCode, filterCode, filterContainSub);
+  const { treeData: filterTreeData } = usePerValueBriefs(filterTypeCode);
   const newFormRef = useRef<ProFormInstance>();
   const editFormRef = useRef<ProFormInstance>();
   const [selectedKeys, setSelectedKeys] = useState<Key[]>([]);
-  const sel = useMemo(() => codes[selectedKeys[0]], [codes, selectedKeys])
+  const sel = useMemo(() => codes[selectedKeys[0]], [codes, selectedKeys]);
   const newVisibleChangeCallback = useCallback(
     (visible) => {
       if (visible) {
@@ -64,9 +63,9 @@ const PerManage: React.FC = () => {
   const editVisibleChangeCallback = useCallback(
     (visible) => {
       if (visible) {
-        getPerValue({typeCode, code: sel?.code}).then(res => {
-          editFormRef.current?.setFieldsValue({...res});
-        })
+        getPerValue({ typeCode, code: sel?.code }).then((res) => {
+          editFormRef.current?.setFieldsValue({ ...res });
+        });
       }
     },
     [typeCode, sel],
@@ -74,74 +73,97 @@ const PerManage: React.FC = () => {
   return (
     <Layout>
       <Content>
-        <ProFormSelect label='权限类型' valueEnum={perTypes} fieldProps={{
-          onSelect: setTypeCode,
-          onDeselect: () => {
-            setTypeCode(undefined)
-          },
-        }}/>
+        <ProFormSelect
+          label="权限类型"
+          valueEnum={perTypes}
+          fieldProps={{
+            onSelect: setTypeCode,
+            onDeselect: () => {
+              setTypeCode(undefined);
+            },
+          }}
+        />
         <Form>
           <ProFormGroup>
-            <ProFormTreeSelect width='md' allowClear disabled={!filterTreeData || filterTreeData.length === 0}
-                               label='过滤'
-                               request={async () => {
-                                 return filterTreeData
-                               }} params={{
-              filterTreeData
-            }}
-                               fieldProps={{
-                                 onSelect: setFilterCode,
-                                 onClear: () => {
-                                   setFilterCode(undefined)
-                                 },
-                               }}/>
-            <ProFormSwitch label="包含子" name="filterContainSub" fieldProps={{
-              onChange: setFilterContainSub,
-            }}/>
+            <ProFormTreeSelect
+              width="md"
+              allowClear
+              disabled={!filterTreeData || filterTreeData.length === 0}
+              label="过滤"
+              request={async () => {
+                return filterTreeData;
+              }}
+              params={{
+                filterTreeData,
+              }}
+              fieldProps={{
+                onSelect: setFilterCode,
+                onClear: () => {
+                  setFilterCode(undefined);
+                },
+              }}
+            />
+            <ProFormSwitch
+              label="包含子"
+              name="filterContainSub"
+              fieldProps={{
+                onChange: setFilterContainSub,
+              }}
+            />
           </ProFormGroup>
         </Form>
 
-        {typeCode && <div className={styles.header}>
-          <span>权限列表</span>
-          <div className={styles.toolbar}>
-            <Button type='link' disabled={!selectedKeys || selectedKeys.length === 0} style={{marginRight: '5px'}}
-                    onClick={() => {
-                      setSelectedKeys([])
-                    }
-                    }>取消选择</Button>
-            <EditForm
-              formRef={newFormRef}
-              title="权限"
-              isEdit={false}
-              onVisibleChange={newVisibleChangeCallback}
-              onFinish={async (formData) => {
-                await savePerValue(formData);
-                refresh();
-                message.success('添加成功！');
-                return true;
-              }}
-            >
-              <PerValueForm type={typeCode ? perTypes[typeCode].text : null} parent={sel}/>
-            </EditForm>
-            <EditForm
-              formRef={editFormRef}
-              title="权限"
-              id={sel?.code}
-              idLabel="编码"
-              isEdit={true}
-              onVisibleChange={editVisibleChangeCallback}
-              onFinish={async (formData) => {
-                await savePerValue(formData);
-                refresh();
-                message.success('更新成功！');
-                return true;
-              }}
-            >
-              <PerValueForm type={typeCode ? perTypes[typeCode].text : null}
-                            parent={sel && sel.parent ? codes[sel.parent] : null} isEdit/>
-            </EditForm>
+        {typeCode && (
+          <div className={styles.header}>
+            <span>权限列表</span>
+            <div className={styles.toolbar}>
+              <Button
+                type="link"
+                disabled={!selectedKeys || selectedKeys.length === 0}
+                style={{ marginRight: '5px' }}
+                onClick={() => {
+                  setSelectedKeys([]);
+                }}
+              >
+                取消选择
+              </Button>
+              <EditForm
+                formRef={newFormRef}
+                title="权限"
+                isEdit={false}
+                onVisibleChange={newVisibleChangeCallback}
+                onFinish={async (formData) => {
+                  await savePerValue(formData);
+                  refresh();
+                  message.success('添加成功！');
+                  return true;
+                }}
+              >
+                <PerValueForm type={typeCode ? perTypes[typeCode].text : null} parent={sel} />
+              </EditForm>
+              <EditForm
+                formRef={editFormRef}
+                title="权限"
+                id={sel?.code}
+                idLabel="编码"
+                isEdit={true}
+                onVisibleChange={editVisibleChangeCallback}
+                onFinish={async (formData) => {
+                  await savePerValue(formData);
+                  refresh();
+                  message.success('更新成功！');
+                  return true;
+                }}
+              >
+                <PerValueForm
+                  type={typeCode ? perTypes[typeCode].text : null}
+                  parent={sel && sel.parent ? codes[sel.parent] : null}
+                  isEdit
+                />
+              </EditForm>
+            </div>
           </div>
-        </div>}
+        )}
         {treeData && treeData.length > 0 && (
           <Tree.DirectoryTree
             selectedKeys={selectedKeys}
